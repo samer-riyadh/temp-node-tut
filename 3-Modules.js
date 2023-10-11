b9116e6e4bc61a4cdcf5d5917a05ec2069884fcf
samer-riyadh <samer.riyadh@hotmail.com>
2023-10-11 15:24:03 +0300

    first commit

---
+++ 3-Modules.js
@@ -0,0 +1,10 @@
+// Modules
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+//sayHi(names.samer)
+//sayHi(names.omar)
+
+
+
---
+++ 4-names.js
@@ -0,0 +1,8 @@
+// local
+const john = 'John'
+//share
+const omar = 'omar'
+const samer = 'samer'
+
+module.exports = { omar, samer}
+
